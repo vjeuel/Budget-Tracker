d43a6ccb6442fac350084842c92e600a14c237f8
vjeuel <vjeuel@gmail.com>
2020-08-05 01:35:16 -0500

    new icons, service-worker and manifest added

---
+++ public/service-worker.js
@@ -0,0 +1,55 @@
+const FILES_TO_CACHE = [
+   "/", "./icons/new-icon-144x115.png", "./icons/new-icon-192x154.png",
+   "./icons/new-icon-512x410.png", "/index.html", "/index.js", "/styles.css"
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// Install
+self.addEventListener("install", evt => {
+   evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+         console.log("Your files were pre-cached successfully!");
+         return cache.addAll(FILES_TO_CACHE);
+      })
+   );
+   self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", evt => {
+   evt.waitUntil(
+      caches.keys().then(keyList => {
+         return Promise.all(
+            keyList.map(key => {
+               if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                  console.log("Removing old cache data", key);
+                  return caches.delete(key);
+               }
+            })
+         );
+      })
+   );
+   self.clients.claim();
+});
+
+//fetch
+self.addEventListener("fetch", evt => {
+   if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+         caches.open(DATA_CACHE_NAME).then(cache => {
+            return fetch(evt.request).then(response => {
+               if (response.status === 200) {
+                  cache.put(evt.request.url, response.clone());
+               }
+               return response;
+            })
+               .catch(err => {
+                  return cache.match(evt.request);
+               });
+         }).catch(err => console.log(err))
+      );
+      return;
+   }
+});
